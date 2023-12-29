10e53dcde6646bba8bb26c63901287300409338c
Artur15264 <artur.pires@fiscaltech.com.br>
2023-12-29 14:39:01 -0300

    Adicionando CMake ao programa

---
+++ build/CMakeFiles/simulador.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/simulador.dir/simulacao.cpp.o"
+  "CMakeFiles/simulador.dir/simulacao.cpp.o.d"
+  "simulador"
+  "simulador.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/simulador.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
