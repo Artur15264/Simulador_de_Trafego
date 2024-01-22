bafccd3fde55c3012f6df71a00f5f20aaeab90a7
Artur15264 <artur.pires@fiscaltech.com.br>
2024-01-22 11:55:39 -0300

    Removendo o conteudo de build

--- Simulador/build/CMakeFiles/Simulador_de_Trafego.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for Simulador_de_Trafego.
-# This may be replaced when dependencies are built.
--- tests/build/teste_final[1]_include.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "/home/artur/Treinamento/Simulação_de_Tráfego/tests/build/teste_final[1]_tests.cmake")
-  include("/home/artur/Treinamento/Simulação_de_Tráfego/tests/build/teste_final[1]_tests.cmake")
-else()
-  add_test(teste_final_NOT_BUILT teste_final_NOT_BUILT)
-endif()
